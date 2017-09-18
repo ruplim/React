e5233feda44020b5b524b007b1e41171de82e7c8
ruplim <ruplimbora@gmail.com>
2017-09-18 15:41:35 -0500

    Router.HistoryLocation for clen URL

--- HelloWorld/src/main.js
+++ HelloWorld/src/main.js
@@ -4,6 +4,9 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
+//Add  etra parameter for clean URL
+// http://localhost:9005/authors versus  http://localhost:9005/#/authors
+//Router.run(routes, Router.HistoryLocation, function(Handler) {
 Router.run(routes, function(Handler) {
 	React.render(<Handler/>, document.getElementById('app'));
 });
