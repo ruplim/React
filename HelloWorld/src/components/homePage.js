5920828a2e961a9b10323bcd3d99bf30bb8c0b95
ruplim <ruplimbora@gmail.com>
2017-09-18 12:15:33 -0500

    Inital Commit

---
+++ HelloWorld/src/components/homePage.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+  render: function() {
+
+      return (
+           <div className="jumbotron">
+				<h1>Ruplim Bora</h1>
+				<p>React, React Router, and Flux.</p>
+			</div>
+
+      );
+  }
+});
+
+module.exports = Home;
