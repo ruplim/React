0857beb2352b1a623491ac6aba6613a25dd9194f
ruplim <ruplimbora@gmail.com>
2017-09-18 12:51:02 -0500

    Changes to add Controller View
    Created smart component and dumb component for displaying authors

--- HelloWorld/dist/scripts/bundle.js
+++ HelloWorld/dist/scripts/bundle.js
@@ -46311,20 +46311,9 @@ module.exports = About;
 "use strict";
 
 var React = require('react');
-var AuthorApi = require('../../api/authorApi');
 
-var Authors = React.createClass({displayName: "Authors",
 
-   getInitialState: function() {
-            return {
-                authors: []
-            };
-   },
-
-   componentWillMount: function() {
-       this.setState({authors: AuthorApi.getAllAuthors() });
-   },
-
+var AuthorList = React.createClass({displayName: "AuthorList",
 
     render: function() {
 
@@ -46348,7 +46337,7 @@ var Authors = React.createClass({displayName: "Authors",
                         React.createElement("th", null, "Name")
                     ), 
                     React.createElement("tbody", null, 
-                        this.state.authors.map(createAuthorRow, this)
+                        this.props.authors.map(createAuthorRow, this)
                     )
                )
 
@@ -46358,9 +46347,42 @@ var Authors = React.createClass({displayName: "Authors",
     }
 }); 
 
+module.exports = AuthorList;
+
+},{"react":158}],163:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
+var AuthorApi = require('../../api/authorApi');
+var AuthorList = require('./authorList');
+
+var Authors = React.createClass({displayName: "Authors",
+
+   getInitialState: function() {
+            return {
+                authors: []
+            };
+   },
+
+   componentDidMount: function() {
+       if(this.isMounted()) {
+            this.setState({authors: AuthorApi.getAllAuthors() });
+       }
+   },
+
+
+    render: function() {
+        return (
+            React.createElement("div", null, 
+               React.createElement(AuthorList, {authors: this.state.authors})
+            )
+        );
+    }
+}); 
+
 module.exports = Authors;
 
-},{"../../api/authorApi":159,"react":158}],163:[function(require,module,exports){
+},{"../../api/authorApi":159,"./authorList":162,"react":158}],164:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -46385,7 +46407,7 @@ var Header = React.createClass({displayName: "Header",
 });
 
 module.exports = Header;
-},{"react":158}],164:[function(require,module,exports){
+},{"react":158}],165:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -46405,7 +46427,7 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home;
 
-},{"react":158}],165:[function(require,module,exports){
+},{"react":158}],166:[function(require,module,exports){
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
@@ -46449,4 +46471,4 @@ var Authors = require('./components/authors/authorPage');
 	win.addEventListener('hashchange', render);
 	render();
 })(window);
-},{"./components/about/aboutPage":161,"./components/authors/authorPage":162,"./components/common/header":163,"./components/homePage":164,"jquery":1,"react":158}]},{},[165]);
+},{"./components/about/aboutPage":161,"./components/authors/authorPage":163,"./components/common/header":164,"./components/homePage":165,"jquery":1,"react":158}]},{},[166]);
---
+++ HelloWorld/readme.txt
@@ -0,0 +1,15 @@
+https://github.com/coryhouse/react-flux-starter-kit
+
+npm install -g gulp
+
+npm install --save gulp@3.9.0 gulp-connect@2.2.0 gulp-open@1.0.0
+
+npm install --save browserify@11.0.1 reactify@1.1.1 vinyl-source-stream@1.1.0
+
+npm install --save bootstrap@3.3.5 jquery@2.1.4 gulp-concat@2.6.0
+
+npm install --save gulp-eslint@0.15.0
+
+npm install --save react@0.13.3 react-router@0.13.3 flux@2.0.3
+
+npm install --save lodash
